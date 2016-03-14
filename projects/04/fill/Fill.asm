609abf8b0d7cb0e3e52170a575876065673a320c
Tri Minh Cao <trimcao@gmail.com>
2016-03-14 11:28:02 +0700

    done project 4 - machine language

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -3,9 +3,71 @@
 // by Nisan and Schocken, MIT Press.
 // File name: projects/04/Fill.asm
 
-// Runs an infinite loop that listens to the keyboard input. 
+// Runs an infinite loop that listens to the keyboard input.
 // When a key is pressed (any key), the program blackens the screen,
 // i.e. writes "black" in every pixel. When no key is pressed, the
 // program clears the screen, i.e. writes "white" in every pixel.
 
 // Put your code here.
+
+
+(OUTERLOOP)
+// initialization
+    @SCREEN
+    D = A
+    @addr
+    M = D
+
+    @i
+    M = 0
+
+// listen to keyboard
+    @KBD
+    D = M
+    @WHITEN
+    D; JEQ
+
+(BLACKEN)
+    @i
+    D = M
+    @8192
+    D = D - A
+    @OUTERLOOP
+    D; JEQ
+
+    // fill the screen
+    @addr
+    A = M       // load the address as the address stored in @addr
+    M = -1
+    // increment @addr
+    @i
+    M = M + 1
+    @addr
+    M = M + 1
+    @BLACKEN
+    0; JMP
+
+
+(WHITEN)
+    @i
+    D = M
+    @8192
+    D = D - A
+    @OUTERLOOP
+    D; JEQ
+
+    // fill the screen
+    @addr
+    A = M       // load the address as the address stored in @addr
+    M = 0
+    // increment @addr
+    @i
+    M = M + 1
+    @addr
+    M = M + 1
+    @WHITEN
+    0; JMP
+
+//(END)
+//    @END
+//    0; JMP
--- projects/04/mult/Mult.asm
+++ projects/04/mult/Mult.asm
@@ -7,3 +7,34 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+
+// initialization
+    @i
+    M = 0
+    @R2     // result stored at R2
+    M = 0
+
+(LOOP)
+    // compare i with R0, the loops stops if i == R0
+    @i
+    D = M
+    @R0
+    D = D - M // D = i - R0
+
+    @END
+    D; JEQ
+
+    // increment i
+    @i
+    M = M + 1
+
+    @R1
+    D = M
+    @R2
+    M = M + D
+    @LOOP
+    0; JMP
+
+(END)
+    @END
+    0; JMP
